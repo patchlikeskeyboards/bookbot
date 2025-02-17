9b01c3882a9a346bb52839233bc400e1bd894ebd
patchlikeskeyboards <patrickhumphreysfilm@gmail.com>
2025-02-17 22:03:16 +0000

    C: adding read and count and gitignore

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+def read():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(file_contents)
+
+def count():
+    count_base = []
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        count_base = file_contents.split()
+        word_count = len(count_base)
+        print(word_count)
+
+def main():
+    read()
+    count()
+
+main()
